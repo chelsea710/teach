df52a0451208c9ee6e421c2ac360da02ca5f0ecb
chelsea710 <489514478@qq.com>
2020-03-11 23:01:19 +0800

    swagger

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageContrillerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageContrillerApi.java
@@ -2,12 +2,17 @@ package com.xuecheng.api.cms;
 
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * 接口類
  * @author LHR
  * @date 20200310
  */
+@Api(value = "cms页面管理接口",description = "cms页面管理接口,提供页面的增,删,改,查")
 public interface CmsPageContrillerApi {
 
     /**
@@ -17,5 +22,10 @@ public interface CmsPageContrillerApi {
      * @param queryPageRequest 查询条件
      * @return 统一返回格式
      */
+    @ApiOperation("分页查询页面列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageNum",value = "页码",required = true,paramType = "path",dataType = "int"),
+            @ApiImplicitParam(name = "pageSize",value = "每页记录数",required = true,paramType = "path",dataType = "int"),
+    })
     public QueryResponseResult findPage(int pageNum, int pageSize, QueryPageRequest queryPageRequest);
 }
